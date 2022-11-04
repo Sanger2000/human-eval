b2def470af3b959e059d9d0ba638903bd85f2c38
Sanger2000 <amanrsanger@gmail.com>
2022-11-04 10:36:45 -0400

    Running all trials

--- run.py
+++ run.py
@@ -66,25 +66,11 @@ def iter_hval():
     return out
 
 def get_results(num_tries=10, model='code-davinci-002'):
-    out_file = OUT_FILE.format(model, num_tries) + "full.json"
-
-    pass_1 = OUT_FILE.format(model, 1)
-    pass_10 = OUT_FILE.format(model, 10)
-    pass_100 = OUT_FILE.format(model, 100)
+    out_file = OUT_FILE.format(model, num_tries)
 
     with open(out_file, 'w') as f:
         pass
-    with open(pass_1, 'w') as f:
-        pass
-    with open(pass_10, 'w') as f:
-        pass
-    with open(pass_100, 'w') as f:
-        pass
-
     out_f = open(out_file, 'a')
-    pass_1_f = open(pass_1, 'a')
-    pass_10_f = open(pass_10, 'a')
-    pass_100_f = open(pass_100, 'a')
 
     def wrapped_future(tid, prompt, future):
         return tid, prompt, future
@@ -103,44 +89,16 @@ def get_results(num_tries=10, model='code-davinci-002'):
         tasks.append((task_id, prompt, completions))
         # tasks.append(wrapped_future(task_id, prompt, future))
 
-        """
-        num_requests = 0
-        start_time = time.time()
-        for idx, comp_time in list(enumerate(completion_times))[::-1]:
-            if start_time - comp_time > 60:
-                del completion_times[idx]
-            else:
-                num_requests += 1
-
-        if num_requests >= 15:
-            # Sleep until we have less than 19 requests in the last minute
-            #time.sleep(60 - (time.time() - completion_times[0]))
-        """
         time.sleep(max(1, 4-(time.time() - start)))
 
         for idx, completion in enumerate(completions):
             out = {'task_id': task_id, 'completion': completion}
-
-            if idx == 0:
-                pass_1_f.write(json.dumps(out) + '\n')
-
-            if idx < 10 and num_tries >= 10:
-                pass_10_f.write(json.dumps(out) + '\n')
-
-            if idx < 100 and num_tries >= 100:
-                pass_100_f.write(json.dumps(out) + '\n')
-
             out_f.write(json.dumps(out) + '\n')
 
 
 
-
-
-    # for future in tqdm.tqdm(asyncio.as_completed(tasks), total=len(tasks)):
-        # task_id, prompt, completions = await future
-    # for task_id, prompt, completions in tasks:
-
-
 if __name__ == '__main__':
-    get_results()
+    get_results(num_tries=10)
+    get_results(num_tries=1)
+    get_results(num_tries=100)
     #asyncio.run(get_results())
